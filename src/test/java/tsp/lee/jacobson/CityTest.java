eccf36980a268f9d8985c00a9747141b91cbd925
Thibaud MORIN <thibaud_morin@icloud.com>
2021-01-04 13:57:23 +0100

    Ajout de tests pour City

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -7,8 +7,54 @@ import org.junit.Test;
 public class CityTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void distanceToSameCity() {
+		City c1 = new City(1,1);
+		City c2 = new City(1,1);
+
+		double actual = c1.distanceTo(c2);
+		double expected = 0;
+
+		assertEquals(actual, expected,0);
+
 	}
 
+	@Test
+	public void distanceToSameX() {
+		City c1 = new City(1,2);
+		City c2 = new City(1,1);
+
+		double actual = c1.distanceTo(c2);
+		double expected = 1;
+
+		assertEquals(actual, expected,0);
+
+	}
+
+	@Test
+ 	public void distanceToSameY() {
+ 		City c1 = new City(1,1);
+ 		City c2 = new City(2,1);
+
+ 		double actual = c1.distanceTo(c2);
+ 		double expected = 1;
+
+ 		assertEquals(actual, expected,0);
+
+	 }
+	 
+	 @Test
+	/**
+	 * Testing cities with differents coordinates
+	 */
+ 	public void distanceToDiffXAndY() {
+ 		City c1 = new City(2,2);
+ 		City c2 = new City(1,1);
+
+ 		double actual = c1.distanceTo(c2);
+ 		double expected = Math.sqrt(2);
+
+ 		assertEquals(actual, expected,0);
+
+ 	}
+	
 }
