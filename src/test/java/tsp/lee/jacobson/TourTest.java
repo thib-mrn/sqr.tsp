b59ed544aed85b84b1d3781e29fb9feceb5ae83a
Thibaud MORIN <thibaud_morin@icloud.com>
2021-01-04 14:29:15 +0100

    Mise à jour TourTest

--- src/test/java/tsp/lee/jacobson/TourTest.java
+++ src/test/java/tsp/lee/jacobson/TourTest.java
@@ -6,9 +6,35 @@ import org.junit.Test;
 
 public class TourTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+/** Création de 3 villes pour réaliser les tests */
+	City c1 = new City(5,4);
+ 	City c2 = new City(5,5);
+ 	City c3 = new City(5,6);
+	 
+	 @Test
+ 	public void getDistanceToSameCity() throws Exception{
+ 		double expected = 0;
+		double actual = c3.distanceTo(c3);
+ 		assertEquals(expected, actual, 0.0);
+ 	}
 
+
+ 	@Test
+ 	public void getDistanceToOnlyOneCity() {
+ 		TourManager.addCity(c1);
+ 		Tour t1 = new Tour();
+ 		t1.setCity(0, c1); 
+
+ 		double actual = t1.getDistance();
+ 		double expected = 0;
+
+ 		assertEquals(expected, actual, 0);
+	 }
+
+	@Test
+ 	public void getDistanceBetweenTwoCities(){
+ 		double expected = 1;
+ 		double actual = c1.distanceTo(c2);
+ 		assertEquals(expected, actual, 0.0);
+ 	}
 }
